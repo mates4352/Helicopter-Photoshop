56ee5aa5f8115d77bd1767b789a45327b203337f
Sergey <cabyrkih@mail.ru>
2021-04-02 10:48:45 +0300

    fix

--- src/components/block/gallery/Gallery.jsx
+++ src/components/block/gallery/Gallery.jsx
@@ -10,19 +10,18 @@ export const Gallery = () => {
 			<div className={s.container}>
 				<div className={s.grid}>
 					<h2 className={s.title}>Фотосессия в вертолете</h2>
-					<div className={s.description}>
-						<img src={img} alt='Gallery' className={s.img} />
-						<p className={s.text}>Полет на вертолете это прекрасная возможность получить новые ощущения! А наш замечательный фотограф запечатлит лучшие моменты</p>
+					<img src={img} alt='Gallery' className={s.img} />
+					<div>
+					<p className={s.text}>Полет на вертолете это прекрасная возможность получить новые ощущения! А наш замечательный фотограф запечатлит лучшие моменты</p>
+						<BlockLabels
+							label0={data[0]}
+							label1={data[1]}
+							label2={data[2]}
+							label3={data[3]}
+							label4={data[4]}
+							label5={data[5]}
+						/>
 					</div>
-					<BlockLabels
-						label0={data[0]}
-						label1={data[1]}
-						label2={data[2]}
-						label3={data[3]}
-						label4={data[4]}
-						label5={data[5]}
-					/>
-				
 				</div>
 			</div>
 		</section>
